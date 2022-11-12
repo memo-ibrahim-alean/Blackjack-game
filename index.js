14e6a6c3d87e7fad3eba645a93ecc36b43119f5e
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-11-12 22:08:13 +0200

    Complete getRandomNumber function

--- index.js
+++ index.js
@@ -11,7 +11,16 @@ let cardsEl = document.getElementById("cards-el");
 
 // return a random number between 1 and 13
 function getRandomCard() {
-  return Math.floor(Math.random() * 13) + 1; 
+  // if 1     -> return 11
+  // if 11-13 -> return 10
+  let randomNumer = Math.floor(Math.random() * 13) + 1;
+  if (randomNumer > 10) {
+    return 10;
+  } else if (randomNumer === 1) {
+    return 11;
+  } else {
+    return randomNumer;
+  }
 }
 
 function startGame() {
