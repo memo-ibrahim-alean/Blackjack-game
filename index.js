1438048b27d19e565fd05ac588ff3a536708b1fc
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-11-12 22:31:56 +0200

    Use an object to store player data

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+let player = {
+  name: prompt("Enter Player Name !"),
+  Chips: 145,
+};
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.Chips;
+
 function getRandomCard() {
   let randomNumer = Math.floor(Math.random() * 13) + 1;
   if (randomNumer > 10) {
