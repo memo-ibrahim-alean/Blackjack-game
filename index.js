a28a8c66e5dae09bdd2c9d0fe4b941d0ddf46ae1
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-11-12 13:56:33 +0200

    Display Sum

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 10;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
 
 function startGame() {
+  sumEl.textContent = "Sum: " + sum;
+
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
