0f1d5d99b1445483b70717db0c61a13de0533752
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-11-12 10:54:06 +0200

    Update isAlive variable

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ let firstCard = 4;
 let secondCard = 40;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
-let isAlive = false;
+let isAlive = true;
 
 if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂");
-  isAlive = true;
 } else if (sum === 21) {
   console.log("Wohoo! You've got Blackjack! 🥳");
   hasBlackJack = true;
 } else {
   console.log("You're out of the game! 😭");
+  isAlive = false;
 }
 
 console.log(isAlive);
