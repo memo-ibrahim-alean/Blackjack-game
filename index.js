6a85bf4faa793f0a2d92f11fe765d202a6ad5f7d
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-11-12 14:06:16 +0200

    Display the cards

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
 function startGame() {
   sumEl.textContent = "Sum: " + sum;
@@ -21,4 +22,5 @@ function startGame() {
   }
 
   messageEl.textContent = message;
+  cardsEl.textContent = `Cards : ${firstCard} , ${secondCard}`;
 }
