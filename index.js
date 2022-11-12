a937d508a983636c20ba370ebfc64e2f7c13db6f
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-11-12 22:13:02 +0200

    Assign values in the startGame function

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
-// return a random number between 1 and 13
 function getRandomCard() {
-  // if 1     -> return 11
-  // if 11-13 -> return 10
   let randomNumer = Math.floor(Math.random() * 13) + 1;
   if (randomNumer > 10) {
     return 10;
@@ -24,6 +19,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
@@ -50,6 +50,5 @@ function newCard() {
   let card = getRandomCard();
   sum += card;
   cards.push(card);
-  console.log(cards);
   renderGame();
 }
